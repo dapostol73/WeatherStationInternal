23d9a16b9561cdc665aef4e9fe0017939b889e8b
Demetrius Apostolopoulos <75643490+dapostol73@users.noreply.github.com>
2023-03-17 14:13:09 -0700

    Move OpenWeather settings into ApplicationSettings.h

--- src/ApplicationSettings.h
+++ src/ApplicationSettings.h
@@ -17,6 +17,35 @@ struct WiFiConnection
     }
 };
 
+// OpenWeatherMap Settings
+// Sign up here to get an API key:
+// https://docs.thingpulse.com/how-tos/openweathermap-key/
+// Language code from this list:
+// Arabic - ar, Bulgarian - bg, Catalan - ca, Czech - cz, German - de, Greek - el,
+// English - en, Persian (Farsi) - fa, Finnish - fi, French - fr, Galician - gl,
+// Croatian - hr, Hungarian - hu, Italian - it, Japanese - ja, Korean - kr,
+// Latvian - la, Lithuanian - lt, Macedonian - mk, Dutch - nl, Polish - pl,
+// Portuguese - pt, Romanian - ro, Russian - ru, Swedish - se, Slovak - sk,
+// Slovenian - sl, Spanish - es, Turkish - tr, Ukrainian - ua, Vietnamese - vi,
+// Chinese Simplified - zh_cn, Chinese Traditional - zh_tw.
+struct OpenWeatherInfo
+{
+    bool IsMetric = true;
+    const char *AppID = "";
+    const char *Location = "";
+    const char *Language = "en";
+
+    OpenWeatherInfo() = default;
+
+    OpenWeatherInfo(const char* appID, const char* location, bool isMetric = true, const char* language = "en")
+    {
+        AppID = appID;
+        Location = location;
+        IsMetric = isMetric;
+        Language = language;
+    }
+};
+
 struct ThingSpeakInfo
 {
     int16_t Port = 80;
@@ -36,19 +65,25 @@ struct ThingSpeakInfo
 struct ApplicationSettings
 {
     WiFiConnection WifiSettings;
+    OpenWeatherInfo OpenWeatherSettings;
     ThingSpeakInfo ThingSpeakSettings;
 
     ApplicationSettings() = default;
 
-    ApplicationSettings(WiFiConnection wifiSettings, ThingSpeakInfo thingSpeakSettings)
+    ApplicationSettings(WiFiConnection wifiSettings, OpenWeatherInfo openWeatherSettings, ThingSpeakInfo thingSpeakSettings)
     {
         WifiSettings = wifiSettings;
+        OpenWeatherSettings = openWeatherSettings;
         ThingSpeakSettings = thingSpeakSettings;
     }
 };
 
-ApplicationSettings Home(WiFiConnection("homessid", "homepw123"), ThingSpeakInfo("homeWriteAPIKey", "homeReadAPIKey"));
-ApplicationSettings Office(WiFiConnection("officessid", "officepw123"), ThingSpeakInfo("officeWriteAPIKey", "officeReadAPIKey"));
+ApplicationSettings Home(WiFiConnection("homessid", "homepw123"),
+                         OpenWeatherInfo("homeAppID", "homeLocationID"),
+                         ThingSpeakInfo("homeWriteAPIKey", "homeReadAPIKey"));
+ApplicationSettings Office(WiFiConnection("officessid", "officepw123"),
+                           OpenWeatherInfo("officeAppID", "officeLocationID"),
+                           ThingSpeakInfo("officeWriteAPIKey", "officeReadAPIKey"));
 
 ApplicationSettings AppSettings[] = { Home, Office };
 uint8_t AppSettingsCount = 2;
