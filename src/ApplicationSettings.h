e6189c5d09cf812709e9755a06ac5b5c3a7e26ba
Demetrius Apostolopoulos <75643490+dapostol73@users.noreply.github.com>
2023-03-17 13:27:17 -0700

    Adding ApplicationSettings.h file

---
+++ src/ApplicationSettings.h
@@ -0,0 +1,56 @@
+#ifndef _APP_SETTINGS_
+#define _APP_SETTINGS_
+
+struct WiFiConnection
+{
+    const char* SSID = "Invalid";
+    const char* Password = "Unknown";
+    int16_t Strength = -100;
+    bool Avialable = 0;
+
+    WiFiConnection() = default;
+
+    WiFiConnection(const char* ssid, const char* password)
+    {
+        SSID = ssid;
+        Password = password;
+    }
+};
+
+struct ThingSpeakInfo
+{
+    int16_t Port = 80;
+    const char *Host = "api.thingspeak.com"; //IP address of the thingspeak server
+    const char *APIKeyWrite = ""; //Your own thingspeak write api_key
+    const char *APIKeyRead = "";	 //Your own thingspeak read api_key
+
+    ThingSpeakInfo() = default;
+
+    ThingSpeakInfo(const char* apiKeyWrite, const char* apiKeyRead)
+    {
+        APIKeyWrite = apiKeyWrite;
+        APIKeyRead = apiKeyRead;
+    }
+};
+
+struct ApplicationSettings
+{
+    WiFiConnection WifiSettings;
+    ThingSpeakInfo ThingSpeakSettings;
+
+    ApplicationSettings() = default;
+
+    ApplicationSettings(WiFiConnection wifiSettings, ThingSpeakInfo thingSpeakSettings)
+    {
+        WifiSettings = wifiSettings;
+        ThingSpeakSettings = thingSpeakSettings;
+    }
+};
+
+ApplicationSettings Home(WiFiConnection("homessid", "homepw123"), ThingSpeakInfo("homeWriteAPIKey", "homeReadAPIKey"));
+ApplicationSettings Office(WiFiConnection("officessid", "officepw123"), ThingSpeakInfo("officeWriteAPIKey", "officeReadAPIKey"));
+
+ApplicationSettings AppSettings[] = { Home, Office };
+uint8_t AppSettingsCount = 3;
+
+#endif
