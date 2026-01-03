55d054ba3f1e4890cc7d23052548a86f127199fc
Demetrius Apostolopoulos <dapostol73@hotmail.com>
2026-01-03 16:39:38 -0400

    Add display-specific calibration parameters
    Updated cali_para.h to define calibration parameters based on the display type (ILI9341, ILI9488, or default). This improves accuracy by using the correct values for each supported display.

--- lib/LCDWIKI_TOUCH/cali_para.h
+++ lib/LCDWIKI_TOUCH/cali_para.h
@@ -1,14 +1,25 @@
 #ifndef _CALI_PARA_
 #define _CALI_PARA_
 
-//for resolution 240x320,the calibration parameter is 663,-13,894,-30
-//for resolution 320x480,the calibration parameter is 852,-14,1284,-30
-//for resolution 480x800,the calibration parameter is 1344,-33,2156,-45
+//for resolution ILI9341 240x320,the calibration parameter is 663,-13,894,-30
+//for resolution ILI9488, ST7796S 320x480,the calibration parameter is 852,-14,1284,-30
+//for resolution NT35510 480x800,the calibration parameter is 1344,-33,2156,-45
 
-
-#define XFAC      1307 //663 //852
-#define XOFFSET   -30//(-13) //(-14)
-#define YFAC      2062//894 //1284
-#define YOFFSET   -17//(-30) 
+#ifdef DISPLAY_ILI9341
+	#define XFAC      663
+	#define XOFFSET   -13
+	#define YFAC      894
+	#define YOFFSET   -30
+#elif DISPLAY_ILI9488
+	#define XFAC      852
+	#define XOFFSET   -14
+	#define YFAC      1284
+	#define YOFFSET   -30
+#else
+	#define XFAC      1307
+	#define XOFFSET   -30
+	#define YFAC      2062
+	#define YOFFSET   -17
+#endif
 
 #endif
