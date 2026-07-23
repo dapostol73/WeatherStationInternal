bac3039d697909205befd70b75b26f08ba0fc4de
Demetrius Apostolopoulos <dapostol73@hotmail.com>
2026-07-23 12:23:36 -0300

    Update copyright notice
    Update copyright year from 2018 to 2020 and change author from ThingPulse Ltd. to Demetrius Apostolopoulos.

--- lib/CanadianHyrdographicService/CanadianHydrograpicTidesHiLo.cpp
+++ lib/CanadianHyrdographicService/CanadianHydrograpicTidesHiLo.cpp
@@ -1,6 +1,6 @@
 /**The MIT License (MIT)
  
- Copyright (c) 2018 by ThingPulse Ltd., https://thingpulse.com
+ Copyright (c) 2020 by Demetrius Apostolopoulos
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
